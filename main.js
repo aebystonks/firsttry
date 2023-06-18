4744e324fc5e20a704beff8fe6d198c816301c7d
aebystonks <grimesjohnnn@gmail.com>
2023-06-18 16:51:23 +0600

    second commit

--- main.js
+++ main.js
@@ -1,43 +1,12 @@
-// undefined 
+//calculator
 
-let haveNoValue;
-const copyEmpty = haveNoValue;
-console.log(`Getting undefined: \n ${haveNoValue} \n`)
-
-// Null
-
-const n = "a".match("b");
-
-console.log(`Null: \n ${n} \n`)
-
-
-// boolean
-let bool = 1;
-bool = Boolean(bool)
-
-const copyBool = bool;
-
-const f = 1;
-const s = 2;
-const trueResult = f < s;
-
-console.log(`True moment: \n method first: ${copyBool} \r method second: ${trueResult} \n`)
-
-
-
-
-// NaN
-let u = undefined;
-u = Number(u)
-
-const copyUndefined = u;
-
-const str1 = 10;
-const str2 = '10wtf'
-const result = +str1 + +str2;
-
-console.log(
-	`Nope \n the first method(with undefined): ${copyUndefined} \r
-		second method(with string): ${result}`
-);
-
+const getValue = prompt("Type first number");
+if(!isNaN(getValue)){
+	const getValue2 = prompt("Type second number");
+	if(!isNaN(getValue2)){
+		alert(+getValue + +getValue2);
+	}
+	else{
+		alert("the value isn't number")
+	}
+};
